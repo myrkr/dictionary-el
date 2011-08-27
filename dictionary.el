f749655b76725df9d62e602ff97744510af858a2
Torsten Hilbrich <torsten.hilbrich@gmx.net>
2011-08-27 21:00:31 +0200

    call make-local-hook only under XEmacs
    GNU Emacs deprecated it back in version 21, and will retire it altogether
    in the upcoming version 24.
    
    Fix taken from the Debian package.

--- dictionary.el
+++ dictionary.el
@@ -330,7 +330,8 @@ by the choice value:
   (make-local-variable 'dictionary-default-dictionary)
   (make-local-variable 'dictionary-default-strategy)
   
-  (make-local-hook 'kill-buffer-hook)
+  (if (featurep 'xemacs)
+      (make-local-hook 'kill-buffer-hook))
   (add-hook 'kill-buffer-hook 'dictionary-close t t)
   (run-hooks 'dictionary-mode-hook))
 
